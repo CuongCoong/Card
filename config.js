e25a7f637cb01c9168a5787ca636bcfc8147611b
CuongCoong <congngocuong1506@gmail.com>
2023-03-01 19:21:44 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,14 @@
+const nameGirl = "Ú òa";
+const giftUrl = "https://www.facebook.com/kinhongA3";
+const eventName = "Click me";
+const titleCard = "Gửi tặng Duyên nhé";
+const contentCard =
+	"Gửi Duyên nhé ! Do Duyên phải đi gấp quá nên Ka làm không được đẹp cho lắm. Sang bên trời tây rồi , phải tự làm mọi thứ một mình, rất cô đơn và nhớ nhà , nhưng hay cố gắng học tập và làm việc kiếm ít vốn về sau tự về làm chủ . Nói chung Ka và mọi người ở nhà cũng chỉ support tinh thần được cho Duyên như này thôi . <br> Cố lên nhé =))) ";
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ:
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = "ava.jpg";
+const base64 = "";
+const giftImageBase64 = "data:image/png;base64, " + base64;
