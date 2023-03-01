72b5a71e3f27dfea3d0420e9fccd04c8e5e5e4e6
CuongCoong <congngocuong1506@gmail.com>
2023-03-01 19:31:54 +0700

    fix

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const giftUrl = "https://www.facebook.com/kinhongA3";
 const eventName = "Click me";
 const titleCard = "Gửi tặng Duyên nhé";
 const contentCard =
-	"Gửi Duyên nhé ! Do Duyên phải đi gấp quá nên Ka làm không được đẹp cho lắm. Sang bên trời tây rồi , phải tự làm mọi thứ một mình, rất cô đơn và nhớ nhà , nhưng hay cố gắng học tập và làm việc kiếm ít vốn về sau tự về làm chủ . Nói chung Ka và mọi người ở nhà cũng chỉ support tinh thần được cho Duyên như này thôi . <br> Cố lên nhé =))) ";
+	"Gửi Duyên nhé ! Do Duyên phải đi gấp quá nên Ka làm không được đẹp cho lắm. Sang bên trời tây rồi , phải tự làm mọi thứ một mình, rất cô đơn và nhớ nhà , nhưng hay cố gắng học tập và làm việc kiếm ít vốn về sau tự về làm chủ . Nói chung Ka và mọi người ở nhà cũng chỉ support tinh thần được cho Duyên như này thôi . <br> Cố lên nhé =))) <br> Mọi người yêu Duyên";
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ:
